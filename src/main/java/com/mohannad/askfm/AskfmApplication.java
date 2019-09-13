9040c1b8d1165200fb354a5fbe6076f73355ad0f
mohannad <mohanad2021996@gmail.com>
2019-09-13 20:37:33 +0200

    init ask fm project

---
+++ src/main/java/com/mohannad/askfm/AskfmApplication.java
@@ -0,0 +1,13 @@
+package com.mohannad.askfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskfmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskfmApplication.class, args);
+    }
+
+}
