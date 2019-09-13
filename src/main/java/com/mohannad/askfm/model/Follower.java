a5a4a5fcf61386c0c9fab2a2bbcecba55b67d263
mohannad <mohanad2021996@gmail.com>
2019-09-13 23:23:51 +0200

    create Follower model

---
+++ src/main/java/com/mohannad/askfm/model/Follower.java
@@ -0,0 +1,12 @@
+package com.mohannad.askfm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * created by mohannad  on 13/09/19
+ */
+@Entity
+public class Follower extends BaseEntity {
+
+}
