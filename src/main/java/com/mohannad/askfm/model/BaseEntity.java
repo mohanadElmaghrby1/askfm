4be36f3b3d7d23a3c8ec62be8eaf6d1eda24f2ff
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-09-15 17:39:28 +0300

    baseEntity fix bo identifier

--- src/main/java/com/mohannad/askfm/model/BaseEntity.java
+++ src/main/java/com/mohannad/askfm/model/BaseEntity.java
@@ -3,11 +3,14 @@ package com.mohannad.askfm.model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
 /**
  * created by mohannad  on 13/09/19
  */
-public class BaseEntity {
+@MappedSuperclass
+public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
