6c9962a2295903fda231b3813eae65ec15c38678
mohanad010050 <mohanad2021996@gmail.com>
2019-09-24 14:53:22 -0700

    add database login with spring security

---
+++ src/main/java/com/mohannad/askfm/config/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.mohannad.askfm.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Qualifier("userDetailService")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home","/index.html").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+        //for h2 console to work
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+//    /**
+//     * for in memory authentication
+//     * @return InMemoryUserDetailsManager of user
+//     */
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
+    /**
+     * for database authentication
+     * @return provider of logged in user
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        //set the userDetails service
+        provider.setUserDetailsService(userDetailsService);
+        //set the encryption algorithm
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+}
