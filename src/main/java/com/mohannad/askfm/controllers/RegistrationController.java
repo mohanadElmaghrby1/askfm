9b367d3f558c491321baf878c401f58bf0505f30
mohanad010050 <mohanad2021996@gmail.com>
2019-09-25 15:14:42 -0700

    Add Validation for registration form

--- src/main/java/com/mohannad/askfm/controllers/RegistrationController.java
+++ src/main/java/com/mohannad/askfm/controllers/RegistrationController.java
@@ -4,10 +4,13 @@ import com.mohannad.askfm.commands.UserCommand;
 import com.mohannad.askfm.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -24,8 +27,22 @@ public class RegistrationController {
         return "sign-up";
     }
 
-    @PostMapping("/user")
-    public String createNewUser(@ModelAttribute UserCommand userCommand){
+    /**
+     * object marked up with @Valid to gather the attributes filled out in the form you’re about to build.
+     * @param userCommand
+     * @param bindingResult
+     * @return
+     */
+    @PostMapping("/sign-up")
+    public String createNewUser(@Valid @ModelAttribute("user") UserCommand userCommand , BindingResult bindingResult){
+        //check if there are  errors when user logged in
+        if (bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {
+                System.out.println(objectError.toString());
+            });
+            //return sign-up page again
+            return "sign-up";
+        }
         System.out.println(userCommand);
         userService.saveUserCommand(userCommand);
         return "redirect:/login";
