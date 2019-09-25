4857491cf230c024ab73d5f6b88056a263f5312f
mohanad010050 <mohanad2021996@gmail.com>
2019-09-24 18:19:12 -0700

    create userCommand Object

---
+++ src/main/java/com/mohannad/askfm/commands/UserCommand.java
@@ -0,0 +1,109 @@
+package com.mohannad.askfm.commands;
+
+public class UserCommand {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private Integer birthDay;
+    private Integer birthMonth;
+    private Integer birthYear;
+    private String gender;
+    private String language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Integer birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public Integer getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(Integer birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCommand{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", birthDay=" + birthDay +
+                ", birthMonth=" + birthMonth +
+                ", birthYear=" + birthYear +
+                ", gender='" + gender + '\'' +
+                ", language='" + language + '\'' +
+                '}';
+    }
+}
