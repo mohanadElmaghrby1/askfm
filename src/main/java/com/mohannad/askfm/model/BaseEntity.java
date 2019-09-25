a5bf7daaba79a85b2e27689641446d968c7bd88f
mohanad010050 <mohanad2021996@gmail.com>
2019-09-24 18:21:50 -0700

    Edit id value to Long

--- src/main/java/com/mohannad/askfm/model/BaseEntity.java
+++ src/main/java/com/mohannad/askfm/model/BaseEntity.java
@@ -14,13 +14,13 @@ public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
