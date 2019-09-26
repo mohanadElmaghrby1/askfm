35984901b097c588226e6120da889ebf15930379
mohanad010050 <mohanad2021996@gmail.com>
2019-09-26 12:05:47 -0700

    create profile controller

--- src/main/java/com/mohannad/askfm/controllers/ProfileController.java
+++ src/main/java/com/mohannad/askfm/controllers/ProfileController.java
@@ -1,6 +1,7 @@
 package com.mohannad.askfm.controllers;
 
 import com.mohannad.askfm.commands.UserCommand;
+import com.mohannad.askfm.commands.UserDetailsCommand;
 import com.mohannad.askfm.convertors.UserCommandToUser;
 import com.mohannad.askfm.convertors.UserToUserCommand;
 import com.mohannad.askfm.model.User;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class ProfileController {
@@ -26,7 +29,7 @@ public class ProfileController {
     }
 
     @RequestMapping({"/{username}"})
-    public String deleteRecipe(@PathVariable String username , Model model){
+    public String getProfile(@PathVariable String username , Model model){
         //check if user is correct
         User user =  userService.findByUserName(username);
         UserCommand userCommand = userToUserCommand.convert(user);
@@ -35,8 +38,9 @@ public class ProfileController {
     }
 
     @GetMapping("follow/{id}")
-    public String follow(@PathVariable String id, Model model){
+    public String follow(@PathVariable String id ,Principal principal){
         //make logged in user follow this user
+        System.out.println(principal.toString());
         System.out.println(id);
         return "profile";
     }
