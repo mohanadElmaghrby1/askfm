73162e2f872761387a2301e9cb1e46a81da03570
mohanad010050 <mohanad2021996@gmail.com>
2019-09-25 19:50:33 -0700

    create follow method

--- src/main/java/com/mohannad/askfm/controllers/ProfileController.java
+++ src/main/java/com/mohannad/askfm/controllers/ProfileController.java
@@ -7,6 +7,7 @@ import com.mohannad.askfm.model.User;
 import com.mohannad.askfm.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,13 @@ public class ProfileController {
         User user =  userService.findByUserName(username);
         UserCommand userCommand = userToUserCommand.convert(user);
         model.addAttribute("user" , userCommand);
+        return "profile";
+    }
+
+    @GetMapping("follow/{id}")
+    public String follow(@PathVariable String id, Model model){
+        //make logged in user follow this user
+        System.out.println(id);
         return "profile";
     }
 
