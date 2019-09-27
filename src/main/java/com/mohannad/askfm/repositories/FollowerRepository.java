b9adfe730012dad505f7da718699be21902f2154
mohannad <mohanad2021996@gmail.com>
2019-09-27 10:25:48 +0200

    Create findByUserAndFollower method

--- src/main/java/com/mohannad/askfm/repositories/FollowerRepository.java
+++ src/main/java/com/mohannad/askfm/repositories/FollowerRepository.java
@@ -1,10 +1,14 @@
 package com.mohannad.askfm.repositories;
 
 import com.mohannad.askfm.model.Follower;
+import com.mohannad.askfm.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * created by mohannad on 15/09/2019
  */
 public interface FollowerRepository extends CrudRepository<Follower , Long> {
+    Follower findByUserAndFollower(User user , User follower);
 }
