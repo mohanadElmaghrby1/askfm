e8f4fb78269bca9e1ca87e140988e017c85d9b64
mohannad <mohanad2021996@gmail.com>
2019-09-27 10:28:09 +0200

    Add Follow and UnFollow feature

--- src/main/java/com/mohannad/askfm/commands/UserDetailsCommand.java
+++ src/main/java/com/mohannad/askfm/commands/UserDetailsCommand.java
@@ -58,4 +58,12 @@ public class UserDetailsCommand implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/mohannad/askfm/controllers/LoginController.java
+++ src/main/java/com/mohannad/askfm/controllers/LoginController.java
@@ -3,12 +3,19 @@ package com.mohannad.askfm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class LoginController {
 
 
     @RequestMapping("/login")
-    public String getLogin(){
+    public String getLogin(Principal principal){
+
+        //if user already logged in display home page
+        if (principal != null) {
+            return "redirect:/home";
+        }
         return "sign-in";
     }
 
