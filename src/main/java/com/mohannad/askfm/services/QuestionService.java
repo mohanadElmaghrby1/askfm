3d0186756662975f9ef33910b326228dbabd29c3
mohannad <mohanad2021996@gmail.com>
2019-09-28 17:49:12 +0200

    Create Question Service

---
+++ src/main/java/com/mohannad/askfm/services/QuestionService.java
@@ -0,0 +1,10 @@
+package com.mohannad.askfm.services;
+
+import com.mohannad.askfm.model.Question;
+
+/**
+ * created by mohannad  on 27/09/19
+ */
+public interface QuestionService {
+    Question save(Question question);
+}
