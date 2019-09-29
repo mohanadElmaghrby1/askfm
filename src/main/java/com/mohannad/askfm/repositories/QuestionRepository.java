215a385f0a8519b184e8f4f186c1b8364ee255a7
mohanad010050 <mohanad2021996@gmail.com>
2019-09-29 13:54:10 -0700

    Add findByReceiver jpa method

--- src/main/java/com/mohannad/askfm/repositories/QuestionRepository.java
+++ src/main/java/com/mohannad/askfm/repositories/QuestionRepository.java
@@ -1,10 +1,14 @@
 package com.mohannad.askfm.repositories;
 
 import com.mohannad.askfm.model.Question;
+import com.mohannad.askfm.model.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * created by mohannad on 15/09/2019
  */
 public interface QuestionRepository  extends CrudRepository<Question, Long> {
+    List<Question> findByReceiverUser(User user);
 }
