9f82f61a5423194cd6fb2cdb63fc804447fd97ae
mohanad010050 <mohanad2021996@gmail.com>
2019-09-29 13:50:40 -0700

    create question command

---
+++ src/main/java/com/mohannad/askfm/commands/QuestionCommand.java
@@ -0,0 +1,54 @@
+package com.mohannad.askfm.commands;
+
+import com.mohannad.askfm.model.User;
+
+/**
+ * create by mohannad on 9/29/2019
+ */
+public class QuestionCommand {
+    private long id;
+    private String content;
+    private String createDate;
+    private User senderUser;
+    private User receiverUser;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getSenderUser() {
+        return senderUser;
+    }
+
+    public void setSenderUser(User senderUser) {
+        this.senderUser = senderUser;
+    }
+
+    public User getReceiverUser() {
+        return receiverUser;
+    }
+
+    public void setReceiverUser(User receiverUser) {
+        this.receiverUser = receiverUser;
+    }
+}
