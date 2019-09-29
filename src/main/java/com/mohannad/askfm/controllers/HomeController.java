337b38952e94c3ad525fe3e2a932c26a058f19b5
mohanad010050 <mohanad2021996@gmail.com>
2019-09-29 13:51:58 -0700

    add / mapping

--- src/main/java/com/mohannad/askfm/controllers/HomeController.java
+++ src/main/java/com/mohannad/askfm/controllers/HomeController.java
@@ -1,16 +1,21 @@
 package com.mohannad.askfm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
 /**
  * create by mohannad on 9/25/2019
  */
 @Controller
 public class HomeController {
 
-    @GetMapping({"/home", "/index.html"})
+    @GetMapping({"/","/home", "/index.html"})
     public String getHomePage(){
         return "index";
     }
+
 }
