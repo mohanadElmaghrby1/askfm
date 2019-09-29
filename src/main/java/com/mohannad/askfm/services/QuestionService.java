a623e2c161ce5fe84b81fc31ae2180b120f26e91
mohanad010050 <mohanad2021996@gmail.com>
2019-09-29 13:54:46 -0700

    Create findAllAskedQuestion for specific user

--- src/main/java/com/mohannad/askfm/services/QuestionService.java
+++ src/main/java/com/mohannad/askfm/services/QuestionService.java
@@ -1,10 +1,14 @@
 package com.mohannad.askfm.services;
 
 import com.mohannad.askfm.model.Question;
+import com.mohannad.askfm.model.User;
+
+import java.util.List;
 
 /**
  * created by mohannad  on 27/09/19
  */
 public interface QuestionService {
     Question save(Question question);
+    List<Question> findAllAskedQuestion(User user);
 }
--- src/main/java/com/mohannad/askfm/services/QuestionServiceImpl.java
+++ src/main/java/com/mohannad/askfm/services/QuestionServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mohannad.askfm.services;
 
 import com.mohannad.askfm.model.Question;
+import com.mohannad.askfm.model.User;
 import com.mohannad.askfm.repositories.QuestionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * created by mohannad  on 27/09/19
@@ -20,4 +23,11 @@ public class QuestionServiceImpl implements QuestionService {
     public Question save(Question question) {
         return questionRepository.save(question);
     }
+
+    @Override
+    public List<Question> findAllAskedQuestion(User user) {
+        return questionRepository.findByReceiverUser(user);
+    }
+
+
 }
