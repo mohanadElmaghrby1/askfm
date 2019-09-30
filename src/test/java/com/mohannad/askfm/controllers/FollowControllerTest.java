05425128c3d30677f65e3e8dee3aa048459c49c4
mohanad010050 <mohanad2021996@gmail.com>
2019-09-30 13:24:18 -0700

    Testing FollowController

---
+++ src/test/java/com/mohannad/askfm/controllers/FollowControllerTest.java
@@ -0,0 +1,55 @@
+package com.mohannad.askfm.controllers;
+
+import com.mohannad.askfm.model.Follower;
+import com.mohannad.askfm.model.User;
+import com.mohannad.askfm.services.FollowerService;
+import com.mohannad.askfm.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.jws.soap.SOAPBinding;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class FollowControllerTest {
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    FollowerService followerService;
+
+    MockMvc mockMvc;
+
+    FollowController followController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        followController=new FollowController(userService, followerService);
+        mockMvc = MockMvcBuilders.standaloneSetup(followController).build();
+    }
+
+    @Test
+    void followAndUnFollow() throws Exception {
+        Follower followerRelations = new Follower();
+        //create followed user
+        User followed = new User();
+        followed.setUsername("samy");
+        followerRelations.setUser(followed);
+        when(userService.findByUserName(any())).thenReturn(followed);
+        mockMvc.perform(get("/follow/samy"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("redirect:/samy"));
+
+    }
+}
