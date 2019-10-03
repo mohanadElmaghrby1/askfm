2058c7ee301740e90f024d678c95dd64f957962b
mohanad010050 <mohanad2021996@gmail.com>
2019-10-03 11:32:51 -0700

    Add Query to get the followed users answers

--- src/main/java/com/mohannad/askfm/repositories/AnswerRepository.java
+++ src/main/java/com/mohannad/askfm/repositories/AnswerRepository.java
@@ -1,10 +1,15 @@
 package com.mohannad.askfm.repositories;
 
 import com.mohannad.askfm.model.Answer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * created by mohannad on 15/09/2019
  */
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
+    @Query(value = "SELECT * from Answer INNER JOIN Follower ON  ANSWER.USER_ID=FOLLOWER.USER_ID", nativeQuery = true)
+    List<Answer> gitFollowedAnswers();
 }
