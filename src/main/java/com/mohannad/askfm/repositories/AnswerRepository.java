16e1ee265796c95c5e272cc65c88ceea0de9a3fa
mohanad010050 <mohanad2021996@gmail.com>
2019-10-03 11:38:46 -0700

    Replace CrudRepository with JpaRepository to support native query

--- src/main/java/com/mohannad/askfm/repositories/AnswerRepository.java
+++ src/main/java/com/mohannad/askfm/repositories/AnswerRepository.java
@@ -1,6 +1,7 @@
 package com.mohannad.askfm.repositories;
 
 import com.mohannad.askfm.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,7 +10,7 @@ import java.util.List;
 /**
  * created by mohannad on 15/09/2019
  */
-public interface AnswerRepository extends CrudRepository<Answer, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
     @Query(value = "SELECT * from Answer INNER JOIN Follower ON  ANSWER.USER_ID=FOLLOWER.USER_ID", nativeQuery = true)
     List<Answer> gitFollowedAnswers();
 }
