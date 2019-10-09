e1f16d55014541ba7244604ab71de35bae50c413
mohanad010050 <mohanad2021996@gmail.com>
2019-10-09 16:55:16 -0700

    Update user details

--- src/main/java/com/mohannad/askfm/controllers/SettingController.java
+++ src/main/java/com/mohannad/askfm/controllers/SettingController.java
@@ -1,9 +1,11 @@
 package com.mohannad.askfm.controllers;
 
 import com.mohannad.askfm.commands.UserCommand;
+import com.mohannad.askfm.convertors.UserCommandToUser;
 import com.mohannad.askfm.convertors.UserToUserCommand;
 import com.mohannad.askfm.model.User;
 import com.mohannad.askfm.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,27 +25,29 @@ public class SettingController {
 
     private UserService userService;
     private UserToUserCommand userToUserCommand;
+    private UserCommandToUser userCommandToUser;
 
-    public SettingController(UserService userService, UserToUserCommand userToUserCommand) {
+    public SettingController(UserService userService, UserToUserCommand userToUserCommand
+            , UserCommandToUser userCommandToUser) {
         this.userService = userService;
         this.userToUserCommand = userToUserCommand;
+        this.userCommandToUser = userCommandToUser;
     }
 
-    @GetMapping({"/setting-profile.html" , "/setting"})
-    public String loadSettings(Model model , Principal principal){
+    @GetMapping({"/setting-profile.html", "/setting"})
+    public String loadSettings(Model model, Principal principal) {
         //load logged in user data
         User loggedInUser = userService.findByUserName(principal.getName());
         UserCommand usrCommand = userToUserCommand.convert(loggedInUser);
-        model.addAttribute("user" , usrCommand);
+        model.addAttribute("user", usrCommand);
         return "setting-profile";
     }
 
 
     @PostMapping("/settings/update")
-    public String updateSettings(@Valid @ModelAttribute("user") UserCommand userCommand, BindingResult bindingResult){
-//        User loggedInUser = userService.findByUserName(userCommand.getUsername());
-//        userCommand.setPassword(loggedInUser.getPassword());
-        userService.saveUserCommand(userCommand);
+    public String updateSettings(@Valid @ModelAttribute("user") UserCommand userCommand
+            , BindingResult bindingResult) {
+        userService.save(userCommandToUser.convert(userCommand));
         return "redirect:/setting";
     }
 }
