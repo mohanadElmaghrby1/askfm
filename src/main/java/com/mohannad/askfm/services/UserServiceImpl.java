ed0e743fd289032050c206ad050c5ddeacc058b0
mohanad010050 <mohanad2021996@gmail.com>
2019-10-09 13:44:51 -0700

    Add Default User Role For all Registered users

--- src/main/java/com/mohannad/askfm/services/UserServiceImpl.java
+++ src/main/java/com/mohannad/askfm/services/UserServiceImpl.java
@@ -4,11 +4,14 @@ import com.mohannad.askfm.commands.UserCommand;
 import com.mohannad.askfm.convertors.UserCommandToUser;
 import com.mohannad.askfm.convertors.UserToUserCommand;
 import com.mohannad.askfm.exceptions.NotFoundException;
+import com.mohannad.askfm.model.Role;
 import com.mohannad.askfm.model.User;
 import com.mohannad.askfm.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
 
 @Service
@@ -18,19 +21,24 @@ public class UserServiceImpl implements UserService {
     private UserToUserCommand userToUserCommand;
     private UserCommandToUser userCommandToUser;
     private PasswordEncoder passwordEncoder;
+    private RoleService roleService;
 
     public UserServiceImpl(UserRepository userRepository, UserToUserCommand userToUserCommand,
-                           UserCommandToUser userCommandToUser , PasswordEncoder passwordEncoder) {
+                           UserCommandToUser userCommandToUser , PasswordEncoder passwordEncoder,RoleService roleService) {
         this.userRepository = userRepository;
         this.userToUserCommand = userToUserCommand;
         this.userCommandToUser = userCommandToUser;
         this.passwordEncoder=passwordEncoder;
+        this.roleService=roleService;
     }
 
     @Override
     public UserCommand saveUserCommand(UserCommand userCommand) {
         userCommand.setPassword(passwordEncoder.encode(userCommand.getPassword()));
         User user = userCommandToUser.convert(userCommand);
+        //set user role
+        Role userRole = roleService.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Collections.singleton(userRole)));
         userRepository.save(user);
         return userToUserCommand.convert(user);
     }
