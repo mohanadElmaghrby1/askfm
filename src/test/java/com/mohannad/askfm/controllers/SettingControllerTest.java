01c7ee03c9fa4c22d2feb964958aec5c6fb94c03
mohanad010050 <mohanad2021996@gmail.com>
2019-10-10 11:07:12 +0200

    Add missing params

--- src/test/java/com/mohannad/askfm/controllers/SettingControllerTest.java
+++ src/test/java/com/mohannad/askfm/controllers/SettingControllerTest.java
@@ -1,6 +1,9 @@
 package com.mohannad.askfm.controllers;
 
+import com.mohannad.askfm.commands.UserCommand;
+import com.mohannad.askfm.convertors.UserCommandToUser;
 import com.mohannad.askfm.convertors.UserToUserCommand;
+import com.mohannad.askfm.model.User;
 import com.mohannad.askfm.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,9 @@ import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,22 +27,28 @@ class SettingControllerTest {
     UserService userService;
     @Mock
     UserToUserCommand userToUserCommand;
+    @Mock
+    UserCommandToUser userCommandToUser;
 
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        settingController = new SettingController(userService,userToUserCommand);
+        settingController = new SettingController(userService,userToUserCommand , userCommandToUser);
         mockMvc = MockMvcBuilders.standaloneSetup(settingController).build();
     }
 
     @Test
-    void updateSettings() throws Exception {
-        mockMvc.perform(post("/setting"))
+    void loadSettings() throws Exception {
+        User user = new User();
+        UserCommand userCommand = new UserCommand();
+        when(userService.findByUserName(any())).thenReturn(user);
+        when(userToUserCommand.convert(any())).thenReturn(userCommand);
+        mockMvc.perform(get("/setting"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("setting-profile"))
-                .andExpect(model().attributeExists("usercommand"));
+                .andExpect(model().attributeExists("user"));
 
     }
 }
